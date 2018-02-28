2cd3e92c160ac9171e27198f5e9625b16f6600c6
Alicia Baker <aliciabaker314@gmail.com>
2018-02-28 14:06:09 -0700

    2nd try

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,13 @@
 class Comment < ApplicationRecord
+  before_save :check_author
   belongs_to :topic
+
+
+  #@comment.save
+  #@comment.update
+  def check_author
+    if self.author.blank?
+      self.author = 'anon coward!'
+    end
+  end
 end
